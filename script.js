1ea74652d362330e1e8c2f6117a86eedd9c311de
VHISTORE <vibemusic1712@gmail.com>
2025-12-26 17:19:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 let count = 0;
 const countElement = document.getElementById('cart-count');
+const cartButton = document.getElementById('cart-button');
 
 document.querySelectorAll('.add-to-cart').forEach(button => {
     button.addEventListener('click', () => {
         count++;
         countElement.innerText = count;
 
+        // Эффект на кнопке карточки
         const originalText = button.innerText;
         button.innerText = 'Added!';
         button.style.backgroundColor = '#27ae60';
         
+        // Эффект "прыжка" корзины
+        cartButton.style.transform = 'scale(1.3) rotate(-10deg)';
+        
         setTimeout(() => {
             button.innerText = originalText;
             button.style.backgroundColor = '';
-        }, 800);
+            cartButton.style.transform = '';
+        }, 400);
     });
 });
