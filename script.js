d9af7ba9aa8e8a22c9e78d054ee853a75c35ef0e
VHISTORE <vibemusic1712@gmail.com>
2025-12-26 16:47:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,23 +4,16 @@ const addButtons = document.querySelectorAll('.add-to-cart');
 
 addButtons.forEach(button => {
     button.addEventListener('click', () => {
-        // Increase cart count
         count++;
         countElement.innerText = count;
 
-        // Visual feedback
         const originalText = button.innerText;
-        button.innerText = 'Added! ✓';
-        button.style.backgroundColor = 'var(--success-color)';
-        button.disabled = true;
-
+        button.innerText = 'Added!';
+        button.style.backgroundColor = '#2ed573';
+        
         setTimeout(() => {
             button.innerText = originalText;
-            button.style.backgroundColor = 'var(--primary-color)';
-            button.disabled = false;
-        }, 800);
-
-        // Log for debugging
-        console.log(`Item added: ${button.getAttribute('data-name')}`);
+            button.style.backgroundColor = '';
+        }, 700);
     });
 });
