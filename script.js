2e383268710ef8bcf202421c72bfae80aedf04c6
VHISTORE <vibemusic1712@gmail.com>
2025-12-26 17:23:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ document.querySelectorAll('.add-to-cart').forEach(button => {
         count++;
         countElement.innerText = count;
 
-        // Эффект на кнопке карточки
+        // Button effect
         const originalText = button.innerText;
         button.innerText = 'Added!';
         button.style.backgroundColor = '#27ae60';
         
-        // Эффект "прыжка" корзины
-        cartButton.style.transform = 'scale(1.3) rotate(-10deg)';
+        // Cart "Pop" animation
+        cartButton.style.transform = 'scale(1.2)';
         
         setTimeout(() => {
             button.innerText = originalText;
